0f624a0b004e267e18d9705b1896cb51a25c91b9
carlen29 <neil.carless@ons.gov.uk>
2021-05-24 13:55:47 +0100

    added linux_audit_cloudwatch_to_firehose_trust

--- linux_audit_cloudwatchlogs.tf
+++ linux_audit_cloudwatchlogs.tf
@@ -9,7 +9,7 @@ resource "aws_cloudwatch_log_stream" "linux_audit_kinesis_logs" {
 resource "aws_cloudwatch_log_subscription_filter" "linux_audit_cloudwatchlogs_to_firehose" {
   count           = length(var.linux_audit_cloudwatch_logs_to_ship)
   name            = "${var.name}-cloudwatchlogs-to-firehose"
-  role_arn        = aws_iam_role.cloudwatch_to_firehose_trust[0].arn
+  role_arn        = aws_iam_role.linux_audit_cloudwatch_to_firehose_trust[0].arn
   destination_arn = aws_kinesis_firehose_delivery_stream.linux_audit_cloudwatchlogs_firehose[0].arn
   log_group_name  = var.linux_audit_cloudwatch_logs_to_ship[count.index]
   filter_pattern  = var.subscription_filter_pattern
