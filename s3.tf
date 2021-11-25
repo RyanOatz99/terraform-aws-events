0eae9873743e3e50dd083cedd6cc22c3f4eef3a1
Rikki Barnes <rikkibar-ons@amazon.co.uk>
2021-11-25 10:09:24 +0000

    apply KMS to firehose buckets

--- s3.tf
+++ s3.tf
@@ -2,14 +2,14 @@ resource "aws_s3_bucket" "events_firehose_backups" {
   bucket = "ons-cia-${var.name}-firehose-backups"
   acl    = "private"
 
-  //  server_side_encryption_configuration {
-  //    rule {
-  //      apply_server_side_encryption_by_default {
-  //        kms_master_key_id = aws_kms_key.events_firehose_backups.arn
-  //        sse_algorithm     = "aws:kms"
-  //      }
-  //    }
-  //  }
+    server_side_encryption_configuration {
+      rule {
+        apply_server_side_encryption_by_default {
+          kms_master_key_id = aws_kms_key.events_firehose_backups.arn
+          sse_algorithm     = "aws:kms"
+        }
+      }
+    }
 }
 
 resource "aws_s3_bucket_public_access_block" "events_firehose_backups" {
@@ -20,10 +20,10 @@ resource "aws_s3_bucket_public_access_block" "events_firehose_backups" {
   restrict_public_buckets = true
 }
 
-//resource "aws_kms_key" "events_firehose_backups" {
-//  description             = "This key is used to encrypt bucket objects in the ons-cia-${var.name}-firehose-backups bucket"
-//  deletion_window_in_days = 14
-//  enable_key_rotation     = true
-//  is_enabled              = true
-//  policy                  = data.aws_iam_policy_document.s3_bucket_cmk.json
-//}
+resource "aws_kms_key" "events_firehose_backups" {
+  description             = "This key is used to encrypt bucket objects in the ons-cia-${var.name}-firehose-backups bucket"
+  deletion_window_in_days = 14
+  enable_key_rotation     = true
+  is_enabled              = true
+  policy                  = data.aws_iam_policy_document.s3_bucket_cmk.json
+}
